2f402d6d40e811eb68c76f0d7f1db0a5fa2318ad
Iceing <1948049031@qq.com>
2015-11-12 16:41:52 +0800

    Initial Commit

---
+++ Swift封装tabBar在OC环境下的代码变化/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swift封装tabBar在OC环境下的代码变化
+//
+//  Created by 蔡跃明 on 15/11/12.
+//  Copyright © 2015年 lm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Swift封装tabBar在OC环境下的代码变化Tests/Swift__tabBar_OC________Tests.m
@@ -0,0 +1,39 @@
+//
+//  Swift__tabBar_OC________Tests.m
+//  Swift封装tabBar在OC环境下的代码变化Tests
+//
+//  Created by 蔡跃明 on 15/11/12.
+//  Copyright © 2015年 lm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Swift__tabBar_OC________Tests : XCTestCase
+
+@end
+
+@implementation Swift__tabBar_OC________Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
